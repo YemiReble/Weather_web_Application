145ca4f6e3e4d8b1392179a462db37d6156cc14a
Abodunrin Sulaimon <abodunrinsulaimon1@gmail.com>
2023-06-28 20:43:59 +0000

    File was Renamed [Use this File to start Flask Application Server]

--- auto_start_server.sh
+++ auto_start_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Bash Script that start the Weather Web Application Server
-# Running on port 5050
+# Running on port 5000
 python3 -m weather_web_flask.weather_flask
 # python3 -m weather_web_flask.server_testing
